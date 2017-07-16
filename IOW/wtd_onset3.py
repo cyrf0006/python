da97554b965766f5d5b4844846fae999adcdd8d2
cyrf0006 <Frederic.Cyr@dfo-mpo.gc.ca>
2017-07-16 04:00:11 -0230

    wtd_onset3.py now without wind

--- IOW/wtd_onset3.py
+++ IOW/wtd_onset3.py
@@ -13,10 +13,9 @@ import matplotlib.dates as mdates # For zoomed rectangle
 XLIM = [pd.Timestamp('2010-03-01 11:00:00'), pd.Timestamp('2010-03-02 1:45:00')]
 XLIM1 = [pd.Timestamp('2010-03-01 11:15:00'), pd.Timestamp('2010-03-01 15:00:00')]
 XLIM2 = [pd.Timestamp('2010-03-01 21:00:00'), pd.Timestamp('2010-03-02 01:00:00')]
-XLIM3 = [pd.Timestamp('2010-03-01 11:30:00'), pd.Timestamp('2010-03-01 14:00:00')]
+#XLIM3 = [pd.Timestamp('2010-03-01 11:30:00'), pd.Timestamp('2010-03-01 14:00:00')]
+XLIM3 = [pd.Timestamp('2010-03-01 11:45:00'), pd.Timestamp('2010-03-01 14:30:00')]
 XLIM4 = [pd.Timestamp('2010-03-01 21:15:00'), pd.Timestamp('2010-03-02 00:30:00')]
-#XLIM21 = [pd.Timestamp('2010-03-02 03:00:00'), pd.Timestamp('2010-03-02 06:00:00')]
-#XLIM4 = [pd.Timestamp('2010-03-02 03:45:00'), pd.Timestamp('2010-03-02 05:30:00')]
 
 fig_name = 'S1_onset3.png'
 #3XLIM2 = [pd.Timestamp('2010-03-01 11:30:00'), pd.Timestamp('2010-03-01 14:00:00')]
@@ -24,7 +23,7 @@ YLIM = [19, 78]
 YLIM1 = [20, 77]
 YLIM2 = [20, 77]
 #YLIM3 = [56, 76]
-YLIM3 = [53, 63]
+YLIM3 = [54, 62]
 YLIM4 = [56, 76]
 
 #### ---------- Meto data --------- ####
@@ -183,40 +182,40 @@ def zoomingBox(ax1, roi, ax2, color='red', linewidth=2, roiKwargs={}, arrowKwarg
 
 #### ------- plot figure -------- ####
 fig = plt.figure(1)
-fig.subplots_adjust(hspace=.5)
-## AX0 - Wind
-ax0 = plt.subplot2grid((10,2), (0, 0), rowspan=1, colspan=2)
-df = pd.DataFrame(wind_mag)
-df = df.set_index('date_time')
+fig.subplots_adjust(hspace=.25)
+## ## AX0 - Wind
+## ax0 = plt.subplot2grid((10,2), (0, 0), rowspan=1, colspan=2)
+## df = pd.DataFrame(wind_mag)
+## df = df.set_index('date_time')
 
-ax0.plot(df.index, df.wind_mag, 'b')
-ax0.set_xlim(XLIM[0], XLIM[1])
-ax0.tick_params(labelbottom='off')
-ax0.xaxis.label.set_visible(False)
-ax0.xaxis.set_major_locator(hours2)
-ax0.xaxis.set_major_formatter(dfmt)
-ax0.set_ylabel(r'$\rm \overline{u_w} (m s^{-1})$', color='b')
-ax0.set_ylim(0,20)
-ax0.tick_params('y', colors='b')
-plt.grid()
-ax0.text(XLIM[1], 15, 'a  ', horizontalalignment='right', verticalalignment='center', fontsize=15, color='k')
+## ax0.plot(df.index, df.wind_mag, 'b')
+## ax0.set_xlim(XLIM[0], XLIM[1])
+## ax0.tick_params(labelbottom='off')
+## ax0.xaxis.label.set_visible(False)
+## ax0.xaxis.set_major_locator(hours2)
+## ax0.xaxis.set_major_formatter(dfmt)
+## ax0.set_ylabel(r'$\rm \overline{u_w} (m s^{-1})$', color='b')
+## ax0.set_ylim(0,20)
+## ax0.tick_params('y', colors='b')
+## plt.grid()
+## ax0.text(XLIM[1], 15, 'a  ', horizontalalignment='right', verticalalignment='center', fontsize=15, color='k')
 
-ax01 = ax0.twinx()
-df2 = pd.DataFrame(wave_height)
-df2 = df2.set_index('date_time')
-ax01.plot(df2.index, df2.waveheight_signif, 'r')
-ax01.set_xlim(XLIM[0], XLIM[1])
-ax01.tick_params(labelbottom='off')
-ax01.xaxis.set_major_locator(hours2)
-ax01.xaxis.set_major_formatter(dfmt)
-ax01.set_ylabel(r'$\rm H_s (m)$', color='r')
-ax01.set_ylim(0,5)
-ax01.xaxis.label.set_visible(False)
-ax01.tick_params('y', colors='r')
+## ax01 = ax0.twinx()
+## df2 = pd.DataFrame(wave_height)
+## df2 = df2.set_index('date_time')
+## ax01.plot(df2.index, df2.waveheight_signif, 'r')
+## ax01.set_xlim(XLIM[0], XLIM[1])
+## ax01.tick_params(labelbottom='off')
+## ax01.xaxis.set_major_locator(hours2)
+## ax01.xaxis.set_major_formatter(dfmt)
+## ax01.set_ylabel(r'$\rm H_s (m)$', color='r')
+## ax01.set_ylim(0,5)
+## ax01.xaxis.label.set_visible(False)
+## ax01.tick_params('y', colors='r')
 
 
 ## AX1 - Velocity full
-ax1 = plt.subplot2grid((10, 2), (1, 0), rowspan=3, colspan=2)
+ax1 = plt.subplot2grid((3, 2), (0, 0), rowspan=1, colspan=2)
 levels = np.linspace(-.005, .005, 11)
 c = plt.contourf(Wbin.index, Wbin.columns, Whigh, levels, cmap=plt.cm.RdBu_r, extend="both")
 levels2 = np.linspace(0,10, 5)
@@ -229,7 +228,7 @@ ax1.invert_yaxis()
 ax1.xaxis.set_major_locator(hours2)
 ax1.xaxis.set_major_formatter(dfmt)
 ax1.set_xlim(XLIM[0], XLIM[1])
-#ax1.set_xlabel(XLIM[0].strftime('%d-%b-%Y'))
+ax1.set_xlabel(XLIM[0].strftime('%d-%b-%Y'))
 #ax1.text(XLIM[0], 25, r"  $\rm w' ( m s^{-1})$", horizontalalignment='left', verticalalignment='center', fontsize=14, color='k', fontweight='bold')
 ## # Add dashed rectangle for zoom
 start1 = mdates.date2num(XLIM1[0])
@@ -244,7 +243,7 @@ rect2_y = [YLIM2[1], YLIM2[1], YLIM2[0], YLIM2[0], YLIM2[1]]
 ax1.plot(rect2_x, rect2_y, '--', color='deeppink', linewidth=2)
 
 ## AX11 - Velocity ZOOM 1
-ax11 = plt.subplot2grid((10, 2), (4, 0), rowspan=3, colspan=1)
+ax11 = plt.subplot2grid((3, 2), (1, 0), rowspan=1, colspan=1)
 levels = np.linspace(-.005, .005, 11)
 c = plt.contourf(Wbin.index, Wbin.columns, Whigh, levels, cmap=plt.cm.RdBu_r, extend="both")
 levels2 = np.linspace(0,10, 5)
@@ -268,11 +267,11 @@ ax11.plot(rect3_x, rect3_y, '--', color='deeppink', linewidth=2)
 # Langmuir propagation
 propax = [pd.Timestamp('2010-03-01 12:16:00'), pd.Timestamp('2010-03-01 12:37:00')]
 propay = [30, 60]
-ax11.plot(propax, propay, '--', color='deeppink', linewidth=2)
+ax11.plot(propax, propay, '--', color='k', linewidth=2)
 
 
 ## AX12 - Temperature Zoom
-ax12 = plt.subplot2grid((10, 2), (7, 0), rowspan=3, colspan=1)
+ax12 = plt.subplot2grid((3, 2), (2, 0), rowspan=1, colspan=1)
 levels3 = np.linspace(0,10, 21)
 c = plt.contourf(Tbin.index, Tbin.columns, Tbin.T, levels3, cmap=plt.cm.RdBu_r)
 ax12.set_xlim(XLIM3[0], XLIM3[1])
@@ -287,13 +286,13 @@ ax12.xaxis.set_minor_locator(min15_3)
 
 
 ## AX21 - Velocity ZOOM 2
-ax21 = plt.subplot2grid((10, 2), (4, 1), rowspan=3, colspan=1)
+ax21 = plt.subplot2grid((3, 2), (1, 1), rowspan=1, colspan=1)
 levels = np.linspace(-.005, .005, 11)
 c = plt.contourf(Wbin.index, Wbin.columns, Whigh, levels, cmap=plt.cm.RdBu_r, extend="both")
 ct = plt.contour(Tbin.index, Tbin.columns, Tbin.T, levels2, colors='k', linewidth=0.05)
 ax21.set_xlim(XLIM2[0], XLIM2[1])
 ax21.set_ylim(YLIM2[0], YLIM2[1])
-ax21.tick_params(labelleft='off')
+#ax21.tick_params(labelleft='on')
 ax21.tick_params(labelbottom='on')
 #ax21.set_ylabel(r'Depth (m)')
 ax21.invert_yaxis()
@@ -310,12 +309,12 @@ ax21.plot(rect4_x, rect4_y, '--', color='deeppink', linewidth=2)
 
 # Colorbar for velocity
 ax21.text(XLIM2[1], -25.5, r" $\rm w' ( m s^{-1})$", horizontalalignment='left', verticalalignment='center', fontsize=14, color='k', fontweight='bold')
-cax = plt.axes([0.91,0.38,0.014,0.34])
+cax = plt.axes([0.91,0.39,0.014,0.36])
 plt.colorbar(c, cax=cax, ticks=[-.005, -.003, -.001, .001, .003, .005])
 
 
 ## AX22 - Temperature Zoom
-ax22 = plt.subplot2grid((10, 2), (7, 1), rowspan=3, colspan=1)
+ax22 = plt.subplot2grid((3, 2), (2, 1), rowspan=1, colspan=1)
 c = plt.contourf(Tbin.index, Tbin.columns, Tbin.T, levels3, cmap=plt.cm.RdBu_r)
 ax22.set_xlim(XLIM4[0], XLIM4[1])
 ax22.set_ylim(YLIM4[0], YLIM4[1])
@@ -329,7 +328,7 @@ ax22.xaxis.set_minor_locator(min15_4)
 
 
 # Colorbar for temperature
-ax22.text(XLIM4[1], 57, r" $\rm T(^{\circ}C)$", horizontalalignment='left', verticalalignment='center', fontsize=14, color='k', fontweight='bold')
+ax22.text(XLIM4[1], 58, r" $\rm T(^{\circ}C)$", horizontalalignment='left', verticalalignment='center', fontsize=14, color='k', fontweight='bold')
 cax = plt.axes([0.91,0.11,0.014,0.18])
 plt.colorbar(c, cax=cax, ticks=np.linspace(0,10, 6))
 
@@ -341,7 +340,7 @@ zoomingBox(ax21, [rect4_x[0], rect4_x[1], rect4_y[0], rect4_y[1]], ax22, color='
 
 
 #### ---- Save Figure ---- ####
-fig.set_size_inches(w=9, h=10)
+fig.set_size_inches(w=9, h=9)
 fig.set_dpi(300)
 #fig.tight_layout()
 fig.savefig(fig_name)
