b623317ffe7dc2a3fad6a293a953461c1b55d716
cyrf0006 <Frederic.Cyr@dfo-mpo.gc.ca>
2020-09-01 11:29:08 -0230

    lfa*.py

---
+++ nafc/lfa_sst_analysis.py
@@ -0,0 +1,408 @@
+'''
+
+Analysis of SSTs in LFA polygons. 
+See Pickle files generated in lfa_sst_extract.py
+
+This script assumes method=2
+
+data source: https://www.esrl.noaa.gov/psd/data/gridded/data.ncep.reanalysis.surface.html
+https://www.esrl.noaa.gov/psd/cgi-bin/db_search/DBListFiles.pl?did=195&tid=71800&vid=676
+'''
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+from netCDF4 import Dataset
+import shapefile 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import plotly.offline as py_off
+from plotly.graph_objs import *
+import cmocean
+import os
+
+## Load pickled SST per polygons
+df_W = pd.read_pickle('sst_west_coast_method2.pkl')
+df_S = pd.read_pickle('sst_south_coast_method2.pkl')
+df_NE = pd.read_pickle('sst_northeast_coast_method2.pkl')
+df_A = pd.read_pickle('sst_avalon_method2.pkl')
+# Reset index (set time)
+df_W.reset_index(inplace=True)
+df_S.reset_index(inplace=True)
+df_NE.reset_index(inplace=True)
+df_A.reset_index(inplace=True)
+df_W.set_index('time', inplace=True) 
+df_S.set_index('time', inplace=True) 
+df_NE.set_index('time', inplace=True)
+df_A.set_index('time', inplace=True)
+
+# Keep only 1982-2019
+df_W = df_W[df_W.index.year>1981]
+df_NE = df_NE[df_NE.index.year>1981]
+df_S = df_S[df_S.index.year>1981]
+df_A = df_A[df_A.index.year>1981]
+
+
+## Daily averages (get rid of lat/lon columns)
+df_W_daily = df_W.resample('D').mean().sst
+df_NE_daily = df_NE.resample('D').mean().sst
+df_S_daily = df_S.resample('D').mean().sst
+df_A_daily = df_A.resample('D').mean().sst
+# dropna because daily mean re-introduced them
+df_W_daily.dropna(inplace=True)
+df_NE_daily.dropna(inplace=True)
+df_S_daily.dropna(inplace=True)
+df_A_daily.dropna(inplace=True)
+
+
+#### ---- Summer mean Temperature ---- ####
+df_W_molt = df_W_daily[(df_W_daily.index.month>=6) & (df_W_daily.index.month<=9)]
+df_NE_molt = df_NE_daily[(df_NE_daily.index.month>=6) & (df_NE_daily.index.month<=9)]
+df_S_molt = df_S_daily[(df_S_daily.index.month>=6) & (df_S_daily.index.month<=9)]
+df_A_molt = df_A_daily[(df_A_daily.index.month>=6) & (df_A_daily.index.month<=9)]
+# annual average
+df_W_molt = df_W_molt.resample('As').mean()
+df_NE_molt = df_NE_molt.resample('As').mean()
+df_S_molt = df_S_molt.resample('As').mean()
+df_A_molt = df_A_molt.resample('As').mean()
+
+# Concatenate
+df_annual = pd.concat([df_W_molt, df_NE_molt, df_S_molt, df_A_molt], axis=1)
+df_annual.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+df_annual.index = df_annual.index.year
+df_annual.plot()
+
+## Standardized Anomaly
+clim_mean = df_annual[(df_annual.index>=1981) & (df_annual.index<=2010)].mean()
+clim_std = df_annual[(df_annual.index>=1981) & (df_annual.index<=2010)].std()
+anom_std_all = (df_annual-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+
+for name in regions:
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    plt.text(1981, 2.4, r'$\rm \overline{X}_{1981-2010}=$' + str(np.round(clim_mean[name], 1)) + r'$\rm~\pm~$' + str(np.round(clim_std[name], 1)) + r'$\rm ^{\circ}C$' )
+    plt.ylabel('Standardized anomaly')
+    plt.title(name)
+    plt.ylim([-3, 3])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_sst_mean_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all 
+        
+#### ---- Days above 12---- ####
+# keep only above 12
+df_W_daily = df_W_daily[df_W_daily>=12]
+df_NE_daily = df_NE_daily[df_NE_daily>=12]
+df_S_daily = df_S_daily[df_S_daily>=12]
+df_A_daily = df_A_daily[df_A_daily>=12]
+# Year counts
+days_above_W = df_W_daily.resample('As').count()
+days_above_NE = df_NE_daily.resample('As').count()
+days_above_S = df_S_daily.resample('As').count()
+days_above_A = df_A_daily.resample('As').count()
+
+days_above = pd.concat([days_above_W, days_above_NE, days_above_S, days_above_A], axis=1)
+days_above.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+days_above.index = days_above.index.year
+#days_above.plot()
+
+## Standardized Anomaly
+clim_mean = days_above[(days_above.index>=1981) & (days_above.index<=2010)].mean()
+clim_std = days_above[(days_above.index>=1981) & (days_above.index<=2010)].std()
+anom_std_all = (days_above-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+
+for name in regions:
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    plt.text(1981, 2.4, r'$\rm \overline{X}_{1981-2010}=$' + str(np.int(np.round(clim_mean[name]))) + r'$\rm~\pm~$' + str(np.int(np.round(clim_std[name]))) + r' days' )
+    plt.ylabel('Standardized anomaly')
+    plt.title(name)
+    plt.ylim([-3, 3])
+    plt.xlim([1980, 2020])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_days_above12_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all
+
+#### ---- Days above 15---- ####
+# Reload
+## Daily averages (get rid of lat/lon columns)
+df_W_daily = df_W.resample('D').mean().sst
+df_NE_daily = df_NE.resample('D').mean().sst
+df_S_daily = df_S.resample('D').mean().sst
+df_A_daily = df_A.resample('D').mean().sst
+# dropna because daily mean re-introduced them
+df_W_daily.dropna(inplace=True)
+df_NE_daily.dropna(inplace=True)
+df_S_daily.dropna(inplace=True)
+df_A_daily.dropna(inplace=True)
+# keep only above 15
+df_W_daily = df_W_daily[df_W_daily>=15]
+df_NE_daily = df_NE_daily[df_NE_daily>=15]
+df_S_daily = df_S_daily[df_S_daily>=15]
+df_A_daily = df_A_daily[df_A_daily>=15]
+# Year counts
+days_above_W = df_W_daily.resample('As').count()
+days_above_NE = df_NE_daily.resample('As').count()
+days_above_S = df_S_daily.resample('As').count()
+days_above_A = df_A_daily.resample('As').count()
+
+days_above = pd.concat([days_above_W, days_above_NE, days_above_S, days_above_A], axis=1)
+days_above.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+days_above.index = days_above.index.year
+#days_above.plot.bar()
+#plt.grid()
+#plt.title('number of days above 15 degC')
+
+## Standardized Anomaly
+clim_mean = days_above[(days_above.index>=1981) & (days_above.index<=2010)].mean()
+clim_std = days_above[(days_above.index>=1981) & (days_above.index<=2010)].std()
+anom_std_all = (days_above-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+
+for name in regions:
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    if clim_mean[name]>0:
+        plt.text(1981, 2.4, r'$\rm \overline{X}_{1981-2010}=$' + str(np.int(np.round(clim_mean[name]))) + r'$\rm~\pm~$' + str(np.int(np.round(clim_std[name]))) + r' days' )
+    else:
+        plt.text(1981, 2.4, r'N/A')
+    plt.ylabel('Standardized anomaly')
+    plt.title(name)
+    plt.ylim([-3, 3])
+    plt.xlim([1980, 2020])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_days_above15_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all
+
+#### ---- Days above 12 / below 18 ---- ####
+# Reload
+## Daily averages (get rid of lat/lon columns)
+df_W_daily = df_W.resample('D').mean().sst
+df_NE_daily = df_NE.resample('D').mean().sst
+df_S_daily = df_S.resample('D').mean().sst
+df_A_daily = df_A.resample('D').mean().sst
+# dropna because daily mean re-introduced them
+df_W_daily.dropna(inplace=True)
+df_NE_daily.dropna(inplace=True)
+df_S_daily.dropna(inplace=True)
+df_A_daily.dropna(inplace=True)
+# keep only above 12
+df_W_daily = df_W_daily[(df_W_daily>=12) & (df_W_daily<=18)]
+df_NE_daily = df_NE_daily[(df_NE_daily>=12) & (df_W_daily<=18)]
+df_S_daily = df_S_daily[(df_S_daily>=12) & (df_W_daily<=18)]
+df_A_daily = df_A_daily[(df_A_daily>=12) & (df_W_daily<=18)]
+# Year counts
+days_above_W = df_W_daily.resample('As').count()
+days_above_NE = df_NE_daily.resample('As').count()
+days_above_S = df_S_daily.resample('As').count()
+days_above_A = df_A_daily.resample('As').count()
+
+days_above = pd.concat([days_above_W, days_above_NE, days_above_S, days_above_A], axis=1)
+days_above.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+days_above.index = days_above.index.year
+#days_above.plot()
+#plt.grid()
+#plt.title('number of days between 12-18 degC')
+
+## Standardized Anomaly
+clim_mean = days_above[(days_above.index>=1981) & (days_above.index<=2010)].mean()
+clim_std = days_above[(days_above.index>=1981) & (days_above.index<=2010)].std()
+anom_std_all = (days_above-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+
+for name in regions:
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    plt.text(1981, 2.4, r'$\rm \overline{X}_{1981-2010}=$' + str(np.int(np.round(clim_mean[name]))) + r'$\rm~\pm~$' + str(np.int(np.round(clim_std[name]))) + r' days' )
+    plt.ylabel('Standardized anomaly')
+    plt.title(name)
+    plt.ylim([-3, 3])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_days_range12-18_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all 
+    
+    
+#### ---- Week of max temp.---- ####
+df_W_weekly = df_W.resample('W').mean().sst
+df_NE_weekly = df_NE.resample('W').mean().sst
+df_S_weekly = df_S.resample('W').mean().sst
+df_A_weekly = df_A.resample('W').mean().sst
+
+# find max weekly average temperature
+weekly_max_W = df_W_weekly.resample('As').max()
+weekly_max_NE = df_NE_weekly.resample('As').max()
+weekly_max_S = df_S_weekly.resample('As').max()
+weekly_max_A = df_A_weekly.resample('As').max()
+
+weekly_max = pd.concat([weekly_max_W, weekly_max_NE, weekly_max_S, weekly_max_A], axis=1)
+weekly_max.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+weekly_max.index = weekly_max.index.year
+#weekly_max.plot.bar()
+#plt.grid()
+#plt.title('Maximum weekly average temperature')
+
+## Standardized Anomaly
+clim_mean = weekly_max[(weekly_max.index>=1981) & (weekly_max.index<=2010)].mean()
+clim_std = weekly_max[(weekly_max.index>=1981) & (weekly_max.index<=2010)].std()
+anom_std_all = (weekly_max-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+region_name_EN = ['West Coast', 'Avalon', 'Northeast Coast', 'South Coast']
+region_name_FR = ['Côte Ouest', 'Avalon', 'Côte Nord-Ouest', 'Côte Sud']
+
+for idx, name in enumerate(regions):
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    plt.text(1981, 2.4, r'$\rm \overline{X}_{1981-2010}=$' + str(np.round(clim_mean[name], 1)) + r'$\rm~\pm~$' + str(np.round(clim_std[name], 1)) + r'$\rm ^{\circ}C$' )
+    plt.ylabel('Normalized anomaly')
+    plt.title(region_name_EN[idx])
+    plt.ylim([-3, 3])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_weekly_max_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+
+    # Save French Figure
+    plt.ylabel('Anomalie normalisée')
+    plt.title(region_name_FR[idx])
+    fig_name = 'lfa_weekly_max_anomaly_' + name + '_FR.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)   
+    
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all 
+
+
+#### ---- WOY of max temp.---- ####
+df_W_weekly = df_W.resample('W').mean().sst
+df_NE_weekly = df_NE.resample('W').mean().sst
+df_S_weekly = df_S.resample('W').mean().sst
+df_A_weekly = df_A.resample('W').mean().sst
+
+# find week of max average temperature
+weekly_stack_W = df_W_weekly.groupby([(df_W_weekly.index.year), (df_W_weekly.index.weekofyear)]).mean()
+weekly_stack_NE = df_NE_weekly.groupby([(df_W_weekly.index.year), (df_W_weekly.index.weekofyear)]).mean()
+weekly_stack_S = df_S_weekly.groupby([(df_W_weekly.index.year), (df_W_weekly.index.weekofyear)]).mean()
+weekly_stack_A = df_A_weekly.groupby([(df_W_weekly.index.year), (df_W_weekly.index.weekofyear)]).mean()
+weekly_W = weekly_stack_W.unstack()
+weekly_NE = weekly_stack_NE.unstack()
+weekly_S = weekly_stack_S.unstack()
+weekly_A = weekly_stack_A.unstack()
+woy_max_W = weekly_W.idxmax(axis=1)  
+woy_max_NE = weekly_NE.idxmax(axis=1)  
+woy_max_S = weekly_S.idxmax(axis=1)  
+woy_max_A = weekly_A.idxmax(axis=1)  
+
+woy_max = pd.concat([woy_max_W, woy_max_NE, woy_max_S, woy_max_A], axis=1)
+woy_max.columns = ['West_Coast', 'Northeast_Coast', 'South_Coast', 'Avalon']
+
+
+## Standardized Anomaly
+clim_mean = woy_max[(woy_max.index>=1981) & (woy_max.index<=2010)].mean()
+clim_std = woy_max[(woy_max.index>=1981) & (woy_max.index<=2010)].std()
+anom_std_all = (woy_max-clim_mean) / clim_std
+# Plot anomaly
+regions = ['West_Coast', 'Avalon', 'Northeast_Coast', 'South_Coast']
+
+for name in regions:
+    anom_std = anom_std_all[name]
+    df1 = anom_std[anom_std>0]
+    df2 = anom_std[anom_std<0]
+    fig = plt.figure(1)
+    fig.clf()
+    width = .7
+    p1 = plt.bar(df1.index, np.squeeze(df1.values), width, alpha=0.8, color='indianred', zorder=10)
+    p2 = plt.bar(df2.index, np.squeeze(df2.values), width, bottom=0, alpha=0.8, color='steelblue', zorder=10)
+    plt.ylabel('Standardized anomaly')
+    plt.title(name)
+    plt.ylim([-3, 3])
+    plt.grid()
+    # Save Figure
+    fig.set_size_inches(w=7,h=4)
+    fig_name = 'lfa_woy_max_anomaly_' + name + '.png'
+    fig.savefig(fig_name, dpi=300)
+    os.system('convert -trim ' + fig_name + ' ' + fig_name)
+    del df1, df2, anom_std
+
+del  clim_mean, clim_std, anom_std_all
+
+
+os.system('montage lfa_days_above12_anomaly_West_Coast.png lfa_days_above12_anomaly_Northeast_Coast.png lfa_days_above12_anomaly_South_Coast.png lfa_days_above12_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_days_above12.png')
+
+os.system('montage lfa_days_above15_anomaly_West_Coast.png lfa_days_above15_anomaly_Northeast_Coast.png lfa_days_above15_anomaly_South_Coast.png lfa_days_above15_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_days_above15.png')
+
+os.system('montage lfa_days_range12-18_anomaly_West_Coast.png  lfa_days_range12-18_anomaly_Northeast_Coast.png lfa_days_range12-18_anomaly_South_Coast.png lfa_days_range12-18_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_days_range12-18.png')
+
+os.system('montage lfa_weekly_max_anomaly_West_Coast.png lfa_weekly_max_anomaly_Northeast_Coast.png lfa_weekly_max_anomaly_South_Coast.png lfa_weekly_max_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_weekly_max.png')
+
+os.system('montage lfa_weekly_max_anomaly_West_Coast_FR.png lfa_weekly_max_anomaly_Northeast_Coast_FR.png lfa_weekly_max_anomaly_South_Coast_FR.png lfa_weekly_max_anomaly_Avalon_FR.png -tile 2x2 -geometry +20+25  -background white  lfa_weekly_max_FR.png')
+
+os.system('montage lfa_woy_max_anomaly_West_Coast.png lfa_woy_max_anomaly_Northeast_Coast.png lfa_woy_max_anomaly_South_Coast.png lfa_woy_max_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_woy_max.png')
+
+os.system('montage lfa_sst_mean_anomaly_West_Coast.png lfa_sst_mean_anomaly_Northeast_Coast.png lfa_sst_mean_anomaly_South_Coast.png lfa_sst_mean_anomaly_Avalon.png -tile 2x2 -geometry +20+25  -background white  lfa_sst_mean.png')
+
+
---
+++ nafc/lfa_sst_map.py
@@ -0,0 +1,281 @@
+'''
+Attempt to plot a climatological map, but not working at the moment...
+'''
+
+
+# data source: https://www.esrl.noaa.gov/psd/data/gridded/data.ncep.reanalysis.surface.html
+# https://www.esrl.noaa.gov/psd/cgi-bin/db_search/DBListFiles.pl?did=195&tid=71800&vid=676
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import xarray as xr
+from netCDF4 import Dataset
+import shapefile 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import plotly.offline as py_off
+from plotly.graph_objs import *
+import cmocean
+
+# Some parameters
+#lonLims = [-66.5, -47] # Maximum MPA zone
+#latLims = [40, 62]
+lonLims = [-61, -47] # LFAs
+latLims = [43, 53]
+
+lonLims = [-61, -51] # LFAs truncated
+latLims = [46, 53]
+METHOD=2
+
+## --------- Get SSTs -------- ####
+# Load ESA SST
+#ds = xr.open_dataset('/home/cyrf0006/data/SST_ESACCI/ESACCI-GLO-SST-L4-REP-OBS-SST_1570728723070.nc')
+# Load NOAA SST
+#ds = xr.open_mfdataset('/home/cyrf0006/data/NOAA_SST/sst.day.mean*.nc', combine='by_coords')
+print('load mfdataset ...')
+ds = xr.open_mfdataset('/media/cyrf0006/Seagate Backup Plus Drive/SST_NOAA/sst.day.mean.2*.nc', combine='by_coords')
+print(' -> Done!')
+
+# update longitude (WOW!)
+print('Subset data ...')
+ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180))
+# Selection of a subset region
+ds = ds.where((ds.lon>lonLims[0]) & (ds.lon<lonLims[1]), drop=True) # original one
+ds = ds.where((ds.lat>latLims[0]) & (ds.lat<latLims[1]), drop=True)
+print(' -> Done!')
+
+ds_monthly = ds.resample(time="M").mean('time') 
+ds_season = ds.resample(time="Q").mean('time') 
+
+# to DataFrame
+print('to DataFrame...')
+df = ds_season.to_dataframe()
+print(' -> Done!')
+
+# Summer climato
+df_col = df.swaplevel(i='time', j='lat')    
+df_col.dropna(inplace=True)
+# Transfer index to datetime
+df_col = df_col.reset_index()
+df_col.set_index('time', inplace=True)
+df_col.index = pd.to_datetime(df_col.index)
+df_col = df_col[df_col.index.month==9]
+# HERE!!!
+    
+## --------- GetShapefiles -------- ####
+myshp = open('/home/cyrf0006/research/PeopleStuff/CoughlanStuff/LobsterFishingAreas.shp', 'rb')
+mydbf = open('/home/cyrf0006/research/PeopleStuff/CoughlanStuff/LobsterFishingAreas.dbf', 'rb')
+r = shapefile.Reader(shp=myshp, dbf=mydbf)
+records = r.records()
+shapes = r.shapes()
+# Fill dictionary with NAFO divisions
+lobster_area = {}
+for idx, rec in enumerate(records):
+    if rec[-1] == '':
+        continue
+    else:
+        lobster_area[rec[-1]] = np.array(shapes[idx].points)
+
+#lfa = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13A', '13B', '14A', '14B', '14C']                        
+polygon3 = Polygon(lobster_area['3'])
+polygon4 = Polygon(lobster_area['4'])
+polygon5 = Polygon(lobster_area['5'])
+polygon6 = Polygon(lobster_area['6'])
+polygon7 = Polygon(lobster_area['7'])
+polygon8 = Polygon(lobster_area['8'])
+polygon9 = Polygon(lobster_area['9'])
+polygon10 = Polygon(lobster_area['10'])
+polygon11 = Polygon(lobster_area['11'])
+polygon12 = Polygon(lobster_area['12'])
+polygon13A = Polygon(lobster_area['13A'])
+polygon13B = Polygon(lobster_area['13B'])
+polygon14A = Polygon(lobster_area['14A'])
+polygon14B = Polygon(lobster_area['14B'])
+polygon14C = Polygon(lobster_area['14C'])
+
+
+#### --------- Extract SST in polygons -------- ####
+
+print('Select polygons...')
+
+if METHOD == 1:
+# METHOD 1 - Works but long
+    # swap level to column dataframe
+    df_col = df.swaplevel(i='time', j='lat')    
+    df_col.dropna(inplace=True)
+
+    # Transfer index to datetime
+    df_col = df_col.reset_index()
+    df_col.set_index('time', inplace=True)
+    df_col.index = pd.to_datetime(df_col.index)
+
+    # drop winter
+    #df_col = df_col[(df_col.index.month>=4) & (df_col.index.month<=10)]
+
+    # Keep only sst>12degC
+    df_col = df_col[df_col.sst>=8]
+
+    # Find data in polygon
+    idx_W = []
+    idx_S = []
+    idx_NE = []
+    idx_A = []
+
+    for i,sst in enumerate(df_col.sst.values):
+        point = Point(df_col.lon[i], df_col.lat[i])
+
+        if i%100000 == 0:
+            print( str(i) + ' / ' + str(len(df_col)))
+
+        if polygon3.contains(point) | polygon4.contains(point) | polygon5.contains(point) | polygon6.contains(point):
+            idx_NE.append(i)
+        elif polygon7.contains(point) | polygon8.contains(point) | polygon9.contains(point) | polygon10.contains(point):
+            idx_A.append(i)
+        elif polygon11.contains(point) | polygon12.contains(point):
+            idx_S.append(i)
+        elif polygon13A.contains(point) | polygon13B.contains(point) | polygon14A.contains(point) | polygon14B.contains(point) | polygon14C.contains(point):
+            idx_W.append(i)
+    print(' -> Done!')
+
+    
+    # New DataFrame per region            
+    df_W = df_col.iloc[idx_W]
+    df_S = df_col.iloc[idx_S]
+    df_NE = df_col.iloc[idx_NE]
+    df_A = df_col.iloc[idx_A]
+    df_W.to_pickle('sst_west_coast_method1.pkl')
+    df_NE.to_pickle('sst_northeast_coast_method1.pkl')
+    df_S.to_pickle('sst_south_coast_method1.pkl')
+    df_A.to_pickle('sst_avalon_method1.pkl')
+
+    
+elif METHOD == 2:
+    # METHOD 2 - Faster (I hope!)
+    df.dropna(inplace=True)    
+    # remove time from index
+    df_col = df.reset_index(level=2) 
+
+    df_col.time = pd.to_datetime(df_col.time)    
+    
+    # Find data in polygon
+    idx_W = []
+    idx_S = []
+    idx_NE = []
+    idx_A = []
+
+    for i,coords in enumerate(df_col.index):
+        lat, lon  = np.array(coords) 
+        point = Point(lon, lat)
+        if i%100000 == 0:
+            print( str(i) + ' / ' + str(len(df_col)))
+
+        if polygon3.contains(point) | polygon4.contains(point) | polygon5.contains(point) | polygon6.contains(point):
+            idx_NE.append(i)
+        elif polygon7.contains(point) | polygon8.contains(point) | polygon9.contains(point) | polygon10.contains(point):
+            idx_A.append(i)
+        elif polygon11.contains(point) | polygon12.contains(point):
+            idx_S.append(i)
+        elif polygon13A.contains(point) | polygon13B.contains(point) | polygon14A.contains(point) | polygon14B.contains(point) | polygon14C.contains(point):
+            idx_W.append(i)
+            
+    print(' -> Done!')
+
+
+    df_W = df_col.iloc[idx_W]
+    df_S = df_col.iloc[idx_S]
+    df_NE = df_col.iloc[idx_NE]
+    df_A = df_col.iloc[idx_A]
+    df_W.to_pickle('sst_west_coast_method2.pkl')
+    df_NE.to_pickle('sst_northeast_coast_method2.pkl')
+    df_S.to_pickle('sst_south_coast_method2.pkl')
+    df_A.to_pickle('sst_avalon_method2.pkl')    
+
+    
+#### --------- Stats per polygons -------- ####       
+
+
+if METHOD == 1:
+    df_W = pd.read_pickle('sst08_west_coast.pkl')
+    df_S = pd.read_pickle('sst08_south_coast.pkl')
+    df_NE = pd.read_pickle('sst08_northeast_coast.pkl')
+    df_A = pd.read_pickle('sst08_avalon.pkl')
+
+    # Daily averages
+    df_W_daily = df_W.resample('D').mean().sst
+    df_NE_daily = df_S.resample('D').mean().sst
+    df_S_daily = df_NE.resample('D').mean().sst
+    df_A_daily = df_A.resample('D').mean().sst
+
+    # dropna because daily mean re-introduced them
+    df_W_daily.dropna(inplace=True)
+    df_NE_daily.dropna(inplace=True)
+    df_S_daily.dropna(inplace=True)
+    df_A_daily.dropna(inplace=True)
+
+    # keep only above 12
+    df_W_daily = df_W_daily[df_W_daily>=12]
+    df_NE_daily = df_NE_daily[df_NE_daily>=12]
+    df_S_daily = df_S_daily[df_S_daily>=12]
+    df_A_daily = df_A_daily[df_A_daily>=12]
+
+    # Year counts
+    days_above_W = df_W_daily.resample('As').count()
+    days_above_NE = df_NE_daily.resample('As').count()
+    days_above_S = df_S_daily.resample('As').count()
+    days_above_A = df_A_daily.resample('As').count()
+
+    # plot
+    ax = days_above_W.plot()
+    days_above_NE.plot(ax=ax)
+    days_above_S.plot(ax=ax)
+    days_above_A.plot(ax=ax)
+    plt.legend(['West Coast', 'NE Coast', 'South Coast', 'Avalon'])
+
+elif METHOD == 2:
+
+    df_W = pd.read_pickle('sst_west_coast_method2.pkl')
+    df_S = pd.read_pickle('sst_south_coast_method2.pkl')
+    df_NE = pd.read_pickle('sst_northeast_coast_method2.pkl')
+    df_A = pd.read_pickle('sst_avalon_method2.pkl')
+
+    # Reset index (set time)
+    df_W.reset_index(inplace=True)
+    df_S.reset_index(inplace=True)
+    df_NE.reset_index(inplace=True)
+    df_A.reset_index(inplace=True)
+    df_W.set_index('time', inplace=True) 
+    df_S.set_index('time', inplace=True) 
+    df_NE.set_index('time', inplace=True)
+    df_A.set_index('time', inplace=True)
+
+    # Daily averages
+    df_W_daily = df_W.resample('D').mean().sst
+    df_NE_daily = df_S.resample('D').mean().sst
+    df_S_daily = df_NE.resample('D').mean().sst
+    df_A_daily = df_A.resample('D').mean().sst
+
+    # dropna because daily mean re-introduced them
+    df_W_daily.dropna(inplace=True)
+    df_NE_daily.dropna(inplace=True)
+    df_S_daily.dropna(inplace=True)
+    df_A_daily.dropna(inplace=True)
+
+    # keep only above 12
+    df_W_daily = df_W_daily[df_W_daily>=12]
+    df_NE_daily = df_NE_daily[df_NE_daily>=12]
+    df_S_daily = df_S_daily[df_S_daily>=12]
+    df_A_daily = df_A_daily[df_A_daily>=12]
+
+    # Year counts
+    days_above_W = df_W_daily.resample('As').count()
+    days_above_NE = df_NE_daily.resample('As').count()
+    days_above_S = df_S_daily.resample('As').count()
+    days_above_A = df_A_daily.resample('As').count()
+
+    # plot
+    ax = days_above_W.plot()
+    days_above_NE.plot(ax=ax)
+    days_above_S.plot(ax=ax)
+    days_above_A.plot(ax=ax)
+    plt.legend(['West Coast', 'NE Coast', 'South Coast', 'Avalon'])
